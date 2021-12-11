e36c215612d9b1583ae42e0ccbe7c428066d4613
askiaxiaoming <askiaxiaoming@outlook.com>
2021-12-11 19:43:31 +0800

    新增DocumentItem接口

--- src/main/java/tech/argentoaskia/models/interfaces/DocumentItem.java
+++ src/main/java/tech/argentoaskia/models/interfaces/DocumentItem.java
@@ -1,45 +1,35 @@
 package tech.argentoaskia.models.interfaces;
 
 import tech.argentoaskia.models.Item;
-import tech.argentoaskia.models.impl.Document;
-import tech.argentoaskia.models.impl.Key;
-import tech.argentoaskia.models.impl.Session;
-import tech.argentoaskia.models.impl.Value;
 
-import java.io.File;
+import java.io.RandomAccessFile;
 import java.util.HashMap;
-import java.util.Set;
+import java.util.Map;
 
-public abstract class DocumentItem<T> implements Item <File>{
-    protected File path;
-    protected HashMap<T, SessionItem<T>> sessionHashMap;
+public interface DocumentItem<T> extends Item<T> {
 
-    public DocumentItem(File path){
-        this.path = path;
-        sessionHashMap = new HashMap<>();
+    int getSessionCount();
+    <S> SessionItem<S> getSession(Object judgeObj);
+    <S> Map< Object, SessionItem<S> > getAllSessions();
+    default void checkOutOfBounds(int index){
+        int maxCount = getSessionCount();
+        if(index >= maxCount){
+            throw new IndexOutOfBoundsException("文档中最多只有" + maxCount +"个节点，" +
+                    "index必须在：[0-" + (maxCount - 1) + "]");
+        }
+    }
+    default <S> Map< Object, SessionItem<S> > getPartSession(Object ...judgeObjs){
+        HashMap<Object, SessionItem<S>> map = new HashMap<>();
+        for (Object jObj :
+                judgeObjs) {
+            SessionItem<S> sSessionItem = getSession(judgeObjs);
+            if(sSessionItem != null){
+                map.put(jObj, sSessionItem);
+            }
+        }
+        return map;
     }
 
-    public DocumentItem(String strPath){
-        path = new File(strPath);
-        sessionHashMap = new HashMap<>();
-    }
-
-    public Set<T> keySet(){
-        return sessionHashMap.keySet();
-    }
-    public SessionItem<T> getValue(T sessionName){
-        return sessionHashMap.get(sessionName);
-    }
-    public int keyCount(){
-        return sessionHashMap.size();
-    }
-    public SessionItem<T> putValue(SessionItem<T> sessionItem){
-        T sessionName = sessionItem.get();
-        return sessionHashMap.put(sessionName, sessionItem);
-    }
     @Override
-    public File get() {
-        return path;
-    }
-
+    RandomAccessFile output(Object... mode);
 }
