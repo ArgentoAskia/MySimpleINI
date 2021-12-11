a85b40e6f29706e2a8ef3032b8dd9d7ae062f5a8
askiaxiaoming <askiaxiaoming@outlook.com>
2021-12-11 19:47:53 +0800

    新增Session接口

--- src/main/java/tech/argentoaskia/models/interfaces/SessionItem.java
+++ src/main/java/tech/argentoaskia/models/interfaces/SessionItem.java
@@ -1,56 +1,35 @@
 package tech.argentoaskia.models.interfaces;
 
 import tech.argentoaskia.models.Item;
-import tech.argentoaskia.models.impl.Key;
-import tech.argentoaskia.models.impl.Value;
 
 import java.util.HashMap;
-import java.util.Objects;
-import java.util.Set;
+import java.util.Map;
 
-public abstract class SessionItem<T> implements Item<T> {
-    protected HashMap<Key, Value> keyValueHashMap;
-    protected T sessionName;
-    public SessionItem(T nameFlag){
-        sessionName = nameFlag;
-        keyValueHashMap = new HashMap<>();
-    }
+public interface SessionItem<T> extends Item<T> {
 
-    public Set<Key> keySet(){
-        return keyValueHashMap.keySet();
-    }
-    public Value getValue(Key key){
-        return keyValueHashMap.get(key);
-    }
-    public Value putKeyValue(Key key, Value value){
-        return keyValueHashMap.put(key, value);
-    }
-    public int keyCount(){
-        return keyValueHashMap.size();
-    }
-
-    @Override
-    public T get() {
-        return sessionName;
-    }
-
-    @SuppressWarnings("unchecked")
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        if (o.getClass() == getClass()){
-            SessionItem<?> that = (SessionItem<?>) o;
-            return Objects.equals(sessionName, that.sessionName);
-        }else if (o.getClass() == sessionName.getClass()){
-            T that = (T) o;
-            return Objects.equals(sessionName, that);
+    int getKeyValueCount();
+    ValueItem<?> getValueByKey(KeyItem<?> keyItem);
+    KeyItem<?> getKey(Object key);
+    Map< Object, KeyItem<?> > getAllKeys();
+    default Map< Object, KeyItem<?> > getPartKeys(Object ...keys){
+        HashMap<Object, KeyItem<?>> map = new HashMap<>();
+        for (Object key :
+                keys) {
+            KeyItem<?> keyItem = getKey(key);
+            if(keyItem != null){
+                map.put(key, keyItem);
+            }
         }
-        return false;
+        return map;
+    }
+    default void checkOutOfBounds(int index){
+        int maxCount = getKeyValueCount();
+        if(index >= maxCount){
+            throw new IndexOutOfBoundsException("文档中最多只有" + maxCount +"个节点，" +
+                    "index必须在：[0-" + (maxCount - 1) + "]");
+        }
     }
 
     @Override
-    public int hashCode() {
-        return Objects.hash(sessionName);
-    }
+    Object output(Object... params);
 }
