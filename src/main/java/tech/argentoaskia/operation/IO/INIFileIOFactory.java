0d25dec9d8a96d3f57f198ede53a9ccbb55e4e18
askiaxiaoming <askiaxiaoming@outlook.com>
2021-12-11 19:44:38 +0800

    更换包

--- src/main/java/tech/argentoaskia/operation/IO/INIFileIOFactory.java
+++
@@ -1,7 +0,0 @@
-package tech.argentoaskia.operation.IO;
-
-public class INIFileIOFactory {
-    public static <T extends INIFileReader> T createINIReader(){
-        return null;
-    }
-}
---
+++ src/main/java/tech/argentoaskia/operation/IO/factory/INIFileIOFactory.java
@@ -0,0 +1,9 @@
+package tech.argentoaskia.operation.IO.factory;
+
+import tech.argentoaskia.operation.IO.impl.INIFileReaderBaseImpl;
+
+public class INIFileIOFactory {
+    public static <T extends INIFileReaderBaseImpl> T createINIReader(){
+        return null;
+    }
+}
