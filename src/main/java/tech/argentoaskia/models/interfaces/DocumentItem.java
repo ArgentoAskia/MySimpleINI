4af567e39269607a39e11da41affdbcb15c5d966
askiaxiaoming <askiaxiaoming@outlook.com>
2021-12-22 13:02:43 +0800

    使用更加精简的DocumentItem接口

--- src/main/java/tech/argentoaskia/models/interfaces/DocumentItem.java
+++ src/main/java/tech/argentoaskia/models/interfaces/DocumentItem.java
@@ -6,30 +6,10 @@ import java.io.RandomAccessFile;
 import java.util.HashMap;
 import java.util.Map;
 
-public interface DocumentItem<T> extends Item<T> {
+public interface DocumentItem<D> extends Item<D> {
 
     int getSessionCount();
-    <S> SessionItem<S> getSession(Object judgeObj);
-    <S> Map< Object, SessionItem<S> > getAllSessions();
-    default void checkOutOfBounds(int index){
-        int maxCount = getSessionCount();
-        if(index >= maxCount){
-            throw new IndexOutOfBoundsException("文档中最多只有" + maxCount +"个节点，" +
-                    "index必须在：[0-" + (maxCount - 1) + "]");
-        }
-    }
-    default <S> Map< Object, SessionItem<S> > getPartSession(Object ...judgeObjs){
-        HashMap<Object, SessionItem<S>> map = new HashMap<>();
-        for (Object jObj :
-                judgeObjs) {
-            SessionItem<S> sSessionItem = getSession(judgeObjs);
-            if(sSessionItem != null){
-                map.put(jObj, sSessionItem);
-            }
-        }
-        return map;
-    }
-
-    @Override
-    RandomAccessFile output(Object... mode);
+    <S> SessionItem<S> getSession(Object sessionString, Class<S> typeForSessionData);
+    <S> SessionItem<S> getSession(int index, Class<S> typeForSessionData);
+    <S> Map<Object, SessionItem<S>> getAllSessions();
 }
