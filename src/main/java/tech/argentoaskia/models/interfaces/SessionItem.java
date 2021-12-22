68230ade7a157fcbf86aa856dbfa58cccae2f209
askiaxiaoming <askiaxiaoming@outlook.com>
2021-12-22 13:08:33 +0800

    更加简易的SessionItem接口

--- src/main/java/tech/argentoaskia/models/interfaces/SessionItem.java
+++ src/main/java/tech/argentoaskia/models/interfaces/SessionItem.java
@@ -2,34 +2,12 @@ package tech.argentoaskia.models.interfaces;
 
 import tech.argentoaskia.models.Item;
 
-import java.util.HashMap;
-import java.util.Map;
+public interface SessionItem<S> extends Item<S> {
 
-public interface SessionItem<T> extends Item<T> {
+    int count();
+    <K> KeyItem<K> getKey(int index, Class<K> typeForKeyData);
+    <K> KeyItem<K> getKey(Object keyString, Class<K> typeForKeyData);
+    KeyItem<?>[] getAllKeys();
 
-    int getKeyValueCount();
-    ValueItem<?> getValueByKey(KeyItem<?> keyItem);
-    KeyItem<?> getKey(Object key);
-    Map< Object, KeyItem<?> > getAllKeys();
-    default Map< Object, KeyItem<?> > getPartKeys(Object ...keys){
-        HashMap<Object, KeyItem<?>> map = new HashMap<>();
-        for (Object key :
-                keys) {
-            KeyItem<?> keyItem = getKey(key);
-            if(keyItem != null){
-                map.put(key, keyItem);
-            }
-        }
-        return map;
-    }
-    default void checkOutOfBounds(int index){
-        int maxCount = getKeyValueCount();
-        if(index >= maxCount){
-            throw new IndexOutOfBoundsException("文档中最多只有" + maxCount +"个节点，" +
-                    "index必须在：[0-" + (maxCount - 1) + "]");
-        }
-    }
 
-    @Override
-    Object output(Object... params);
 }
