edd76ce4e9b3f72d99260618553e8cee84bdc34d
askiaxiaoming <askiaxiaoming@outlook.com>
2021-12-22 13:08:44 +0800

    更加简易的ValueItem接口

--- src/main/java/tech/argentoaskia/models/interfaces/ValueItem.java
+++ src/main/java/tech/argentoaskia/models/interfaces/ValueItem.java
@@ -2,8 +2,5 @@ package tech.argentoaskia.models.interfaces;
 
 import tech.argentoaskia.models.Item;
 
-public interface ValueItem<T> extends Item<T> {
-
-    @Override
-    Object output(Object... params);
+public interface ValueItem<V> extends Item<V> {
 }
